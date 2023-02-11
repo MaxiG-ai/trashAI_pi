c37e420e1fe90ee167a15224ebede2258453f7bc
Maxi <maxgraf27@gmail.com>
2023-02-11 16:06:03 +0100

    removed problematic images file name

--- takepicture.py
+++ takepicture.py
@@ -12,6 +12,6 @@ button.wait_for_press()
 # Read picture. ret === True on success
 sleep(1)
 ret, frame = video_capture.read()
-cv2.imwrite(f'./img/{datetime.now()}.jpg', frame)
+cv2.imwrite(f'./img/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.jpg', frame)
 # Close device
 video_capture.release()
