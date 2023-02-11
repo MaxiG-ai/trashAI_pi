4d8928fd33a1bf73e6dd971f5c87c042a7c9bbce
Maxi <maxgraf27@gmail.com>
2023-02-11 15:52:44 +0100

    added takepicture.py

---
+++ takepicture.py
@@ -0,0 +1,17 @@
+import cv2
+from gpiozero import Button
+from datetime import datetime
+from time import sleep
+
+button = Button(17)
+video_capture = cv2.VideoCapture(0)
+# Check success
+if not video_capture.isOpened():
+    raise Exception("Could not open video device")
+button.wait_for_press()
+# Read picture. ret === True on success
+sleep(1)
+ret, frame = video_capture.read()
+cv2.imwrite(f'./img/{datetime.now()}.jpg', frame)
+# Close device
+video_capture.release()
